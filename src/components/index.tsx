f61c231ed57151640e7a42ca8a688bdcbb781e5c
ChienChihYeh <miroyeh1986@gmail.com>
2024-01-02 23:34:58 +0800

    Refactor AuthBackgroundDiv style merge

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -50,10 +50,12 @@ export const AuthBackgroundDiv = forwardRef(function (
 ) {
   const imageURL = useFetchImageAndSetURL(url, token, errorCallback)
   const { style } = { ...restProps }
-  const mergedStyle = {
-    ...style,
-    backgroundImage: `url(${imageURL})`,
-  }
+  const mergedStyle = imageURL
+    ? {
+        ...style,
+        backgroundImage: `url(${imageURL})`,
+      }
+    : style
   //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
 
   return (
